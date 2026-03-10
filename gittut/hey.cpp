6a67d663f176580e6628699640feef9abf05154f
Sanjana Shenoy <sanjanashenoy1@gmail.com>
2026-03-10 20:11:32 +0530

    Version 1

---
+++ gittut/hey.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"enter a number";
+    cin>>a;
+    if(a==2)
+    {
+        cout<<"YAY U GO QUEEN";
+    
+    }
+    else
+    cout<<"booo you should have chosen 2";
+}
